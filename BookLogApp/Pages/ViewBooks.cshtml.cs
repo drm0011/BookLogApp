ce5d5034cfc9f59cccaadb282afadbfe5e9834f4
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-11-08 21:26:49 +0100

    view books method

---
+++ BookLogApp/Pages/ViewBooks.cshtml.cs
@@ -0,0 +1,24 @@
+using BookLogAppFactories;
+using BookLogAppInterfaces;
+using DomainModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLogApp.Pages
+{
+    public class ViewBooksModel : PageModel
+    {
+        private readonly IBookBLL _bookBLL;
+
+        public ViewBooksModel()
+        {
+            _bookBLL=BookFactory.CreateBookBLL();
+        }
+
+        public List<Book> Books { get; set; }   
+        public void OnGet()
+        {
+            Books=_bookBLL.GetBooks();
+        }
+    }
+}
