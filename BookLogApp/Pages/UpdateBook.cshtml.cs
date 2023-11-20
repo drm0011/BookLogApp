5cd685edaaed759a8786507c9bf1adf0d33618ba
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-11-20 16:47:33 +0100

    update ui changes

---
+++ BookLogApp/Pages/UpdateBook.cshtml.cs
@@ -0,0 +1,36 @@
+using BookLogAppFactories;
+using BookLogAppInterfaces;
+using DomainModelsLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLogApp.Pages
+{
+    public class UpdateBookModel : PageModel
+    {
+        private readonly IBookBLL _bookBLL;
+
+        public UpdateBookModel()
+        {
+            _bookBLL = BookFactory.CreateBookBLL();
+        }
+
+        [BindProperty] 
+        public Book Book {get; set; } //TODO: create viewmodel for ui?
+        public void OnGet(int id)
+        {
+            Book = _bookBLL.GetBookById(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _bookBLL.UpdateBook(Book);
+            return RedirectToPage("./ViewBooks");
+        }
+    }
+}
