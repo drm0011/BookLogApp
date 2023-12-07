42914526916c32b103b9a72bebd9015def1466a8
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-12-07 23:34:54 +0100

    tests

---
+++ BookLogApp.Tests/LogicTests.cs
@@ -0,0 +1,33 @@
+﻿using BookLogAppBLL;
+using BookLogAppInterfaces;
+using DomainModelsLayer;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLogApp.Tests
+{
+    public class LogicTests
+    {
+        [Fact]
+        public void AssignGenresToBook_ShouldCreateRelation()
+        {
+            Mock<IGenreRepo> mockRepo = new Mock<IGenreRepo>();
+            GenreBLL genreBLL = new GenreBLL(mockRepo.Object);
+
+            Book book = new Book("Book Title", "Book Author", "Book Summary", "12345");
+            Genre genre = new Genre { Name = "Fiction" };
+
+            book.ID = 1;
+            genre.ID = 1;
+
+            genreBLL.CreateBooksGenreRelation(book.ID, genre.ID);
+
+            mockRepo.Verify(repo => repo.CreateBooksGenreRelation(book.ID, genre.ID), Times.Once());
+        }
+    }
+}
+
