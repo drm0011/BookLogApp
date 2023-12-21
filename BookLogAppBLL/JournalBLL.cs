bae8e7df6b2c72edaf992576f689598d32593fd5
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-12-21 04:38:11 +0100

    analyze mood code

--- BookLogAppBLL/JournalBLL.cs
+++ BookLogAppBLL/JournalBLL.cs
@@ -12,9 +12,34 @@ namespace BookLogAppBLL
     public class JournalBLL:IJournalBLL
     {
         private readonly IJournalRepo _journalRepo;
+        private Dictionary<string, List<(string word, int score)>> _moodKeywords;
         public JournalBLL(IJournalRepo journalRepo)
         {
             _journalRepo = journalRepo;
+            _moodKeywords = new Dictionary<string, List<(string word, int score)>>
+            {
+                { "Happy", new List<(string word, int score)> { ("joy", 1), ("happy", 1), ("delighted", 2) } },
+                { "Sad", new List<(string word, int score)> { ("sad", 1), ("depressed", 2), ("unhappy", 1) } },
+            };
+        }
+        public string AnalyzeMood(string journalEntry)
+        {
+            Dictionary<string, int> moodScores = new Dictionary<string, int>();
+
+            foreach (string mood in _moodKeywords.Keys)
+            {
+                moodScores[mood] = 0;
+                foreach ((string word, int score) in _moodKeywords[mood])
+                {
+                    if (journalEntry.IndexOf(word, StringComparison.OrdinalIgnoreCase)>=0)
+                    {
+                        moodScores[mood] += score;
+                    }
+                }
+            }
+
+            string predominantMood = moodScores.OrderByDescending(kv => kv.Value).First().Key;
+            return predominantMood;
         }
         public Journal GetEntryAndBookById(int id, int bookId)
         {
